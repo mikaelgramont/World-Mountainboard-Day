7387f685f1f4589caf6a183278ed2f1a995a661b
Mikael Gramont <mgramont@gmail.com>
2012-03-16 09:06:37 -0700

    Got part of the templates to render on the PHP side: the uc method works but plain tags do not.

--- php/MyMustache.php
+++ php/MyMustache.php
@@ -3,35 +3,22 @@ class MyMustache extends Mustache
 {
 	public function render($template = null, $view = null, $partials = null)
 	{
-		$view->i18n = new MyMustacheI18n($view->i18n);
+		$view->i18n = new MustacheI18n(null, clone($view));
 		
 		return parent::render($template, $view, $partials);
 	}
 }
 	
-class MyMustacheI18n
+class MustacheI18n extends Mustache
 {	
-	protected $_data;
-	
-	public function __construct($data)
+	public function uc()
 	{
-		$this->_data = $data;
-	}
-	
-	public function __get($k)
-	{
-		return (isset($this->_data->$k) ? $this->_data->$k : null);
-	}
-	
-	public function __set($k, $v)
-	{
-		$this->_data->$k = $v;
-	}
-	
-	public function uc($text)
-	{
-		error_log('uc method called with ' . $text);
-		return(strtoupper($text));
+		$m = $this;
+		return function($tag) use ($m) {
+			$ret = $m->render($tag);
+			error_log('uc method called with ' . $tag . ' - ' . $ret);
+			return $ret;
+		};
 	}
 }
 
